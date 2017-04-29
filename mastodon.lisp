7f40a7072c0a6b6587a57ab8c687b49d6e2f32b6
cxxxr <g23tlm@gmail.com>
2017-04-30 00:55:19 +0900

    cleanup

--- mastodon.lisp
+++ mastodon.lisp
@@ -4,34 +4,26 @@
         :mastodon.entity))
 (in-package :mastodon)
 
-(defun api-post-status (app text &key in-reply-to-id media-ids sensitive spoiler-text visibility)
-  (let ((plist
-         (http-post app
-                    "/api/v1/statuses"
-                    `(("status" . ,text)
-                      ,@(when in-reply-to-id
-                          `(("in_reply_to_id" . ,in-reply-to-id)))
-                      ,@(when media-ids
-                          `(("media_ids" . ,media-ids)))
-                      ,@(when sensitive
-                          `(("sensitive" . ,sensitive)))
-                      ,@(when spoiler-text
-                          `(("spoiler_text" . ,spoiler-text)))
-                      ,@(when visibility
-                          `(("visibility" . ,visibility)))))))
-    (parse '<status> plist)))
+;; (defun api-post-status (app text &key in-reply-to-id media-ids sensitive spoiler-text visibility)
+;;   (let ((plist
+;;          (http-post app
+;;                     "/api/v1/statuses"
+;;                     `(("status" . ,text)
+;;                       ,@(when in-reply-to-id
+;;                           `(("in_reply_to_id" . ,in-reply-to-id)))
+;;                       ,@(when media-ids
+;;                           `(("media_ids" . ,media-ids)))
+;;                       ,@(when sensitive
+;;                           `(("sensitive" . ,sensitive)))
+;;                       ,@(when spoiler-text
+;;                           `(("spoiler_text" . ,spoiler-text)))
+;;                       ,@(when visibility
+;;                           `(("visibility" . ,visibility)))))))
+;;     (parse '<status> plist)))
 
-(defun api-follow (app id)
-  (let ((plist (http-post app (format nil "/api/v1/accounts/~A/follow" id))))
-    (parse '<relationship> plist)))
-
-(defun api-unfollow (app id)
-  (let ((plist (http-post app (format nil "/api/v1/accounts/~A/unfollow" id))))
-    (parse '<relationship> plist)))
-
-(defun api-search (app query resolve)
-  (let ((plist (http-get app (format nil "/api/v1/search?q=~A&resolve=~A" query resolve))))
-    (parse '<results> plist)))
+;; (defun api-search (app query resolve)
+;;   (let ((plist (http-get app (format nil "/api/v1/search?q=~A&resolve=~A" query resolve))))
+;;     (parse '<results> plist)))
 
 
 #|
@@ -39,17 +31,17 @@
 * GET /api/v1/accounts/:id
 * GET /api/v1/accounts/verify_credentials
 - PATCH /api/v1/accounts/update_credentials
-- GET /api/v1/accounts/:id/followers
-- GET /api/v1/accounts/:id/following
-- GET /api/v1/accounts/:id/statuses
+* GET /api/v1/accounts/:id/followers
+* GET /api/v1/accounts/:id/following
+* GET /api/v1/accounts/:id/statuses
 * POST /api/v1/accounts/:id/follow
 * POST /api/v1/accounts/:id/unfollow
-- POST /api/v1/accounts/:id/block
-- POST /api/v1/accounts/:id/unblock
-- POST /api/v1/accounts/:id/mute
-- POST /api/v1/accounts/:id/unmute
-- GET /api/v1/accounts/relationships
-- GET /api/v1/accounts/search
+* POST /api/v1/accounts/:id/block
+* POST /api/v1/accounts/:id/unblock
+* POST /api/v1/accounts/:id/mute
+* POST /api/v1/accounts/:id/unmute
+* GET /api/v1/accounts/relationships
+* GET /api/v1/accounts/search
 - POST /api/v1/apps
 - GET /api/v1/blocks
 - GET /api/v1/favourites
